fc70afc59175982287c5232986a4071fbfca0c65
AdmirBajric <60645159+AdmirBajric@users.noreply.github.com>
2020-06-10 09:56:16 +0200

    update

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# Node-Express-Portfolio-Site
- 
+# Node Express Portfolio Site
+
+## Techdegree project 6
+
+### Descrition of the project
+
+1.
+2.
+3.
+
+### Overview of the project
+
+1.
+2.
+3.
+4.
+5.
+6.
+7.
+8.
+9.
+10.
+
+#### More details
+
+##### CSS
+
+1.
+2.
+3.
+4.
--- app.js
+++ app.js
@@ -1,20 +1,55 @@
+// Require app dependencies
 const express = require("express");
 const app = express();
 
+// Require json data
 const { projects } = require("./data.json");
 
+// Set view engine to pug
 app.set("view engine", "pug");
 
+// Set route for static files
 app.use("/static", express.static("public"));
 
+//Configure routes for GET requests
+//Home page
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { projects });
 });
 
+// About page
 app.get("/about", (req, res) => {
   res.render("about");
 });
 
-app.listen(8000, () => {
-  console.log("The application is running on localhost:6000");
+// Project page
+app.get("/projects/:id", (req, res, next) => {
+  const id = req.params.id;
+  // If query is not a integer and when query id is not equal to the projects length array, call next middleware to handle error
+  if (isNaN(id) || id > projects.length - 1) {
+    return next();
+  } else {
+    const project = projects[id];
+    res.render("project", { project });
+  }
+});
+
+// Create custom 404 error and pass it to next middleware
+app.use((req, res, next) => {
+  const err = new Error("Page Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// Display error page when no routes are found
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render("error");
+  console.log(err.message);
+});
+
+// Host app on port
+app.listen(3001, () => {
+  console.log("The application is running on localhost:3000");
 });
